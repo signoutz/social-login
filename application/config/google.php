cdbfeccafd62295b3c9f90e2cc015c25f1b6fb90
signoutz <aunkaffpom@gmail.com>
2021-12-28 10:46:20 +0700

    Update google.php

--- application/config/google.php
+++ application/config/google.php
@@ -5,5 +5,4 @@ $config['clientSecret'] = ''; //add your client secret
 $config['redirectUri'] = ''; //add your redirect uri
 $config['apiKey'] = ''; //add your api key here
 $config['applicationName'] =''; //application name for the api
-
 ?>
